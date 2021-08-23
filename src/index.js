3d04b6b7727cfd8ec662b21a60b986602f6b4722
Iuliia <badmad_91@inbox.ru>
2021-08-23 14:14:01 +0300

    feat: add index file

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (array.lenght && array) {
+    let min = Math.min(...array);
+    let res = [min];
+    return res
+  } else {
+    return 0;
+  }
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array.lenght && array) {
+    let max = Math.max(...array);
+    let result = [max];
+    return result;
+  } else {
+    return 0;
+  }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array.lenght && array) {
+    return array.reduce((a, b) => (a + b)) / array.lenght;
+  } else {
+    return 0;
+  }
 }
